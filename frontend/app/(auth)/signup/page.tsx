54d8128cf5297d7d99db465817e3a5f5a6475d6a
Derik Sehn <derikbosing@gmail.com>
2025-03-31 21:39:54 -0300

    remove auth pages

--- frontend/app/(auth)/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignupForm } from "@/components/auth/signup-form";
-import { NextPage } from "next";
-
-
-export default async function SignupPage({ }: NextPage) {
-    return (<SignupForm />)
-}
